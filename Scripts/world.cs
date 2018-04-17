cbcee97d1930e45de232ce601a414e56aa37f114
DESKTOP-EFN2V20\Walera <test>
2018-04-17 04:31:52 +0300

    Создал файл

---
+++ Scripts/world.cs
@@ -0,0 +1,13 @@
+using System;
+namespace
+{
+	public class Programm
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World");
+			Console.Write("Test");
+			Console.ReadKey();
+		}
+	}
+}
